1477e30cd93b4831ec8ac9ac68eaf334dd2f72b4
Seung-hwan285 <ghks285@naver.com>
2022-01-09 18:40:43 +0900

    feat : 기본적인 디자인

--- movie.js
+++ movie.js
@@ -2,8 +2,8 @@
 // -[x] 영화 정보 api 가져오기
 // -[x] 이미지 api 가져오기 (영화 포스트)
     // -[x] 가져온 영화 정보 api 비동기처리
-    // -[] creatElement로 이미지태그 생성
-    // -[] 이미지 뿌리기
+    // -[x] creatElement로 이미지태그 생성
+    // -[x] 이미지 뿌리기
 
 
 const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
@@ -19,12 +19,9 @@ async function getMovie() {
     console.log(req);
 
 
-    req.results.forEach(movie=>{
-        const img = document.createElement("img");
-        img.src = IMGURL + movie.poster_path;
-        document.body.appendChild(img);
 
-    });
+
+    return req;
 
 }
 
