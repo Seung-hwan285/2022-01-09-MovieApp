60f75972fd28945a74636b86913256ed5b8e83be
Seung-hwan285 <ghks285@naver.com>
2022-01-11 13:08:13 +0900

    refactor: movie.css grid -> flex 수정 header 추가

--- movie.js
+++ movie.js
@@ -20,6 +20,7 @@
     // -[x] 6이상 ORANGE
     // -[x] 나머지 GREEN
 
+
 const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGURL ="https://image.tmdb.org/t/p/w1280";
 
